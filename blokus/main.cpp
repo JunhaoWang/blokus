59c78f5c49933e1ea1cf905cee2cb12aacd70af1
junhao <junhao@umich.edu>
2016-10-28 00:13:34 -0400

    add

--- blokus/main.cpp
+++ blokus/main.cpp
@@ -16,7 +16,7 @@ static const Shape one_cell{Coordinate(0, 0)};
 static const Shape two_cell{Coordinate(0, 0),Coordinate(0, 1)};
 
 
-// TODO: make 21 shapes above
+// TODO: make 21 shapes above here
 
 int main(int argc, const char * argv[]) {
 	
